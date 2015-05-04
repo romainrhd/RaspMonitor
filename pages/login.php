300e8d34ce917992c26946f85f7611eb15c91f40
Romain <romain.richard.19@gmail.com>
2015-05-04 20:59:13 +0200

    Update login.php
    Modification du titre de la page de connexion

--- pages/login.php
+++ pages/login.php
@@ -1,7 +1,7 @@
 <div class="container">
     <div class="center">
         <form role="form" method="post" action="<?php echo LOGIN; ?>">
-        	<h2 class="form-signin-heading">Connexion RaspControl</h2>
+        	<h2 class="form-signin-heading">Connexion RaspMonitor</h2>
             <input type="text" class="form-control" name="username" id="username" placeholder="Nom d'utilsateur" autofocus />
             <input type="password" class="form-control" name="password" id="password" placeholder="Mot de passe" /><br />
             <button type="submit" class="btn btn-lg btn-primary btn-block">Connexion</button><br />
